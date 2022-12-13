d007181b6e3c1280309049636cc38c1aa75c30b1
Timothy Peck <timothy.peck@he-arc.ch>
2022-12-13 15:50:28 +0100

    add base alpha_beta functions

--- ai/Michel_Peck.py
+++ ai/Michel_Peck.py
@@ -20,17 +20,19 @@ class Michel_Peck:
         pass
 
     def next_move(self, board: othello.OthelloGame) -> tuple[int, int]:
-
+        self.board = board
         self.legal_moves = board.get_possible_move()
 
         return self.get_best_move()
 
     def get_best_move(self) -> tuple[int, int]:
         pb = random.random()
-        best_move = (-1, -1)
+        best_move = random.choice(self.legal_moves)
         best_weight = -100
+        first_move = self.board.compute_scores() == (2, 2)
+        print(f"First move: {first_move}")
         for move in self.legal_moves:
-            if pb > self.PB_FEINT:
+            if first_move or pb > self.PB_FEINT:
                 if self.heuristics[move[0]][move[1]] > best_weight:
                     best_weight = self.heuristics[move[0]][move[1]]
                     best_move = move
@@ -39,3 +41,33 @@ class Michel_Peck:
                     best_weight = self.heuristics[move[0]][move[1]]
                     best_move = move
         return best_move
+
+    def ab_max(self, root, parentMin, depth=5):
+        if (depth == 0 or root.final()):
+            return root.eval(), None
+        maxVal = -math.inf
+        maxOp = None
+        for op in root.ops():
+            new = root.apply(op)
+            val, _ = self.ab_min(new, maxVal, depth-1)
+            if val > maxVal:
+                maxVal = val
+                maxOp = op
+                if maxVal > parentMin:
+                    break
+        return maxVal, maxOp
+
+    def ab_min(self, root, parentMax, depth=5):
+        if (depth == 0 or root.final()):
+            return root.eval(), None
+        minVal = -math.inf
+        minOp = None
+        for op in root.ops():
+            new = root.apply(op)
+            val, _ = self.ab_max(new, minVal, depth-1)
+            if val > minVal:
+                minVal = val
+                minOp = op
+                if minVal > parentMax:
+                    break
+        return minVal, minOp
